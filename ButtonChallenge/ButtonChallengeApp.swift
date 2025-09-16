fd57baa2e93cf58c8127ed4cc1a2aec2df098e0e
JanSplat2 <jan.splat@outlook.de>
2025-09-16 14:55:40 -0600

    Initial Commit

---
+++ ButtonChallenge/ButtonChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonChallengeApp.swift
+//  ButtonChallenge
+//
+//  Created by Dittrich, Jan - Student on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ButtonChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
