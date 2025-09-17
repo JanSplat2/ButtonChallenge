35dc46bb84472401ff97f2dad9f7ed7e37db5639
JanSplat2 <jan.splat@outlook.de>
2025-09-16 19:34:45 -0600

    Diffent Buttons, which add on a scale from 1 to 3 to the biggest one and on the cicles which have been pressed. MVP, Stretch one and Stretch thre are done. Stretch three couldn't be finished.

--- ButtonChallenge/ContentView.swift
+++ ButtonChallenge/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Summarize = 0
+    @State private var BlueButton = 0
+    @State private var BlueButtonPressed = false
+    @State private var RedButton = 0
+    @State private var RedButtonPressed = false
+    @State private var GreenButton = 0
+    @State private var GreenButtonPressed = false
+    @State private var UserName = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(UserName)!")
+                .font(.system(size: 40, weight: .bold, design: .rounded))
+                .frame(width: 360, height: 60)
+            Text("\(Summarize)")
+                .font(.system(size: 180, weight: .bold, design: .rounded))
+            TextField("Enter Name", text:  $UserName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .font(.headline)
+                .padding()
+            HStack {
+                Button {
+                    BlueButton += 1
+                    Summarize += 1
+                    BlueButtonPressed.toggle()
+                } label: {
+                    Circle()
+                        .foregroundStyle(.blue)
+                        .overlay {
+                            Text("\(BlueButton)")
+                                .font(.system(size: 72, weight: .bold, design: .rounded))
+                                .foregroundStyle(.white)
+                        }
+                }
+                Button {
+                    GreenButton += 2
+                    Summarize += 2
+                    RedButtonPressed.toggle()
+                }label: {
+                    Circle()
+                        .foregroundStyle(.green)
+                        .overlay {
+                            Text("\(GreenButton)")
+                                .font(.system(size: 72, weight: .bold, design: .rounded))
+                                .foregroundStyle(.white)
+                        }
+                }
+
+                Button {
+                    RedButton += 3
+                    Summarize += 3
+                    GreenButtonPressed.toggle()
+                }label: {
+                    Circle()
+                        .foregroundStyle(.red)
+                        .overlay {
+                            Text("\(RedButton)")
+                                .font(.system(size: 72, weight: .bold, design: .rounded))
+                                .foregroundStyle(.white)
+                        }
+                }
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
